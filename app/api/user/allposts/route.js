b9cf1f307ffaa0275e39bc27b40070032a31a91f
Arijeet10 <arijeetsocial@gmail.com>
2024-04-20 21:45:15 +0530

    like and comment functionality added to posts

--- app/api/user/allposts/route.js
+++ app/api/user/allposts/route.js
@@ -5,6 +5,23 @@ import { NextResponse } from "next/server";
 
 connectMongoDB();
 
+//exporting OPTIONS async function in routes handler 
+export async function OPTIONS(request) {
+    const allowedOrigin = request.headers.get("origin");
+    const response = new NextResponse(null, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": allowedOrigin || "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+        "Access-Control-Max-Age": "86400",
+      },
+    });
+  
+    return response;
+  }
+
 export async function GET(){
     try {
         const posts=await UserPostModel.find();
