401a52009cf145bba9805e9a333755246754faa2
Arijeet10 <arijeetsocial@gmail.com>
2024-04-21 22:37:51 +0530

    loading screen changes

--- components/Loading.js
+++ components/Loading.js
@@ -3,10 +3,9 @@ import { AiFillInstagram } from "react-icons/ai";
 const Loading = () => {
   return (
     <>
-      <div className="w-[300px] absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]">
-        <AiFillInstagram 
-            className="w-full h-full text-pink-500 hover:text-violet-500"
-        />
+      <div className="fixed inset-0 bg-white" />
+      <div className="w-[300px] z-50 absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]">
+        <AiFillInstagram className="w-full h-full text-pink-500 hover:text-violet-500" />
         <div className="text-center font-bold">Loading...</div>
       </div>
     </>
