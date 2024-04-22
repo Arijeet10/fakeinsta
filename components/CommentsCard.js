58b8e0492194d8e9e60838c4c2ffd40c22674643
Arijeet10 <arijeetsocial@gmail.com>
2024-04-22 22:03:36 +0530

    comment changes

--- components/CommentsCard.js
+++ components/CommentsCard.js
@@ -1,18 +1,20 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 
 const CommentsCard = ({ comment }) => {
-
+  const router=useRouter();
   //console.log(comment)  
   
   return (
     <>
       <div className="p-2 border shadow-sm">
-        <div className="flex items-center gap-2">
+        <div onClick={()=>router.push(`/profile/${comment.userID?._id}`)} className="flex items-center gap-2 cursor-pointer">
           <img 
             src={comment.userID?.profilePic}
             alt="profile image"
-            className="w-10 h-10 rounded-full"
+            className="w-10 h-10 rounded-full object-contain border"
           />
           <div>{comment.userID?.fullname}</div>
         </div>
